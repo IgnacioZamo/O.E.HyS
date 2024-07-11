51264486618111aa172782af406ba86dc3fddafa
Ignacio Zamo <ignaciozamo1@gmail.com>
2024-07-10 21:07:39 -0300

    Se instala fontAwesomeIcon y elije iconos para la seccion Service

---
+++ src/components/servicios/Service.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserShield, faHelmetSafety, faPersonDigging } from '@fortawesome/free-solid-svg-icons'
+
+const Service = () => {
+    
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Service
